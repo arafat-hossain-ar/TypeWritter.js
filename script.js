dc194bddc311089a0cc7abc6852e879b73773486
Arafat Hossain Ar <arafat.122260@gmail.com>
2021-05-21 22:53:30 +0600

    code updated

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
+// dom element 
 const textArea = document.querySelector("#typing");
 let cursur = document.querySelector(".cursur");
 let text = textArea.textContent;
 textArea.innerText = "";
+
+// Dynamic Css 
 let dynamicStyles = null;
 function addAnimation(name, body) {
-  if (!dynamicStyles) {
-    dynamicStyles = document.createElement('style');
-    dynamicStyles.type = 'text/css';
-    document.head.appendChild(dynamicStyles);
-  }
-  
-  dynamicStyles.sheet.insertRule(`@keyframes ${ name } {
-    ${ body }
+    if (!dynamicStyles) {
+        dynamicStyles = document.createElement('style');
+        dynamicStyles.type = 'text/css';
+        document.head.appendChild(dynamicStyles);
+    }
+
+    dynamicStyles.sheet.insertRule(`@keyframes ${name} {
+    ${body}
   }`, dynamicStyles.length);
 }
 
@@ -20,6 +23,7 @@ addAnimation('animate', `
   100% { opacity: 1; }
 `);
 
+// Typing System Start 
 let i = 0;
 cursur.style.animation = `animate 300ms infinite`;
 function typeing() {
@@ -30,6 +34,6 @@ function typeing() {
         setTimeout(typeing, type.speed);
     }
 }
-function typed(){
+function typed() {
     setTimeout(() => { typeing(); }, type.delay);
 }
